ca33017e6dd92970ad59b5671915bb10f4c7a5fd
Gregory Mendes <contato@gregorymendes.com>
2017-09-01 19:04:51 -0300

    Input verification

--- src/guessing_game.cr
+++ src/guessing_game.cr
@@ -1,10 +1,19 @@
 require "./guessing_game/*"
 
 module GuessingGame
+  secret_number = rand(100) + 1
   puts "Guess a number!"
   print "Please input your guess:"
   guess = gets
 
   puts "You guessed: #{guess}"
+
+  if guess > secret_number
+    puts "To big!"
+  elsif guess < secret_number
+    puts "To small!"
+  else
+    puts "You wii!"
+  end
 
 end
